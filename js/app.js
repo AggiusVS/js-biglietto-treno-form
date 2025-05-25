21d33e25fafee2b25e8283448957b89d935950dc
AggiusVS <aggiusvellastefano@gmail.com>
2025-05-25 12:31:00 +0200

    better card and js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 
 document.getElementById("ticketForm").addEventListener("submit", function (e) {
     e.preventDefault()
-
-
+    //dichiara le variabili dei dati del passegero
     const km = parseFloat(document.getElementById("km").value)
     const discount = document.getElementById('discount').value;
     const name = document.getElementById("name").value;
     const surname = document.getElementById("surname").value;
 
+    //si assicura che l'utente inserisca un numero valido di km
     if (isNaN(km) || km <= 0) {
         alert("Inserire un numero valido di chilometri");
         return
     }
 
+    //parto con lo sconto a 0 per aumentarlo in base al biglietto selezionato
     const basePrice = km * 0.21
     let discountPerc = 0
 
@@ -21,10 +22,12 @@ document.getElementById("ticketForm").addEventListener("submit", function (e) {
     } else if (discount === "over65") {
         discountPerc = 0.40
     }
-
+    //calcola il prezzo finale
     const discountImport = basePrice * discountPerc
     const finalPrice = basePrice - discountImport
 
+
+    //guarda che tipo di biglietto ha scelto e lo salva in una stringa 
     let discountText = ""
 
     if (discount === "intero") {
@@ -35,11 +38,11 @@ document.getElementById("ticketForm").addEventListener("submit", function (e) {
         discountText = "Sconto studenti (20%)"
     }
 
-    document.getElementById("output").innerHTML = `
-        <strong>Riepilogo:</strong><br> 
-        Nome e Cognome: ${name} ${surname}<br>
-        Chilometri da percorrere: ${km} km<br>
-        Tipo di sconto: ${discountText}<br>
-        Prezzo totale: <strong>€ ${finalPrice.toFixed(2)}</strong>
-    `
+    //rimuove d-none dalle classi di output
+    const output = document.getElementById("output");
+    output.classList.remove("d-none");
+
+    document.getElementById("passengerName").innerText = `${name} ${surname}`;
+    document.getElementById("ticketInfo").innerText = `${discountText}`
+    document.getElementById("ticketPrice").innerText = ` €${finalPrice.toFixed(2)}`
 })
